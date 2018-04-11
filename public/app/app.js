b1a72abb587f6eb1530d8c7cb09e88f8b61a27d3
varesh <gandhamvaresh@gmail.com>
2018-04-11 14:36:28 +0000

    ss

--- public/app/app.js
+++ public/app/app.js
@@ -2,4 +2,4 @@ angular.module('app',['ngResource']);
 angular.module('app').controller('testCtrl',function($scope,$resource) {
     // body...
     $scope.jobs= $resource('/api/jobs').query();
-})
+});
